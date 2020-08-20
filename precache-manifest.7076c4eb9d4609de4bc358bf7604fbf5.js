8705d57a0cf25a51c9bacea61c51b392da3af176
Novan Ardh <NovanArdhn_@live.com>
2020-08-20 19:11:15 +0700

    Updates

---
+++ precache-manifest.7076c4eb9d4609de4bc358bf7604fbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a72121a58f13979529a9ad600d6feed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869ea772cd1b7e5197d4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "07d5b4a34df036327cf9",
+    "url": "/static/js/2.59cd7fe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59cd7fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869ea772cd1b7e5197d4",
+    "url": "/static/js/main.78853850.chunk.js"
+  },
+  {
+    "revision": "b1019e86f67f4652de53",
+    "url": "/static/js/runtime-main.57770fb5.js"
+  }
+]);
